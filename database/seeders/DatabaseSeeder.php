ec23fa2f0cc5b1bf1fc1a86ad811b247425b6205
Raphael Yusuke Nozaki <ryusuken02@gmail.com>
2024-10-10 14:06:02 -0300

    improving Projects factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(200)->create();
-        Project::factory()->count(10)->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(fn (User $u) => Project::factory()->create(['created_by' => $u->id]));
     }
 }
